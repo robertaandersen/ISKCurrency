0cf0d4b7aea95dafa1f35a30924d990cdda36e1c
=robertreynisson@gmail.com <robertreynisson@gmail.com>
2016-02-11 00:56:24 +0000

    RxBus implemented

--- app/src/main/java/is/robertreynisson/iskcurrency/domain_layer/CurrencyViewModel.java
+++ app/src/main/java/is/robertreynisson/iskcurrency/domain_layer/CurrencyViewModel.java
@@ -9,26 +9,62 @@ import is.robertreynisson.iskcurrency.utils.RxBus;
 import is.robertreynisson.iskcurrency.utils.Utils;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.subjects.BehaviorSubject;
-import rx.subjects.PublishSubject;
 import rx.subscriptions.CompositeSubscription;
 
 /**
  * Created by robert on 10.2.2016.
  */
-public class CurrencyViewModel extends AbstractViewModel{
+public class CurrencyViewModel extends AbstractViewModel {
 
     private static final String TAG = CurrencyViewModel.class.getSimpleName();
 
     private final BehaviorSubject<String> time = BehaviorSubject.create();
     private final BehaviorSubject<List<Currency>> currencyList = BehaviorSubject.create();
+    private final static BehaviorSubject<Object> newBaseAmount = BehaviorSubject.create();
+    public static RxBus foreignCurrencyBus = new RxBus();
 
 
     @Override
     protected void subscribeToDataStoreInternal(CompositeSubscription compositeSubscription) {
         Utils.logger(TAG, "Subscribed");
         compositeSubscription.add(MainActivity.serviceAdapter.getRates("m5").map(ModelConverters::currencyModelFromAPI).subscribe(currencyList));
+        compositeSubscription.add(
+                foreignCurrencyBus.toObserverable()
+                        .throttleLast(250, TimeUnit.MILLISECONDS, AndroidSchedulers.mainThread())
+                        .subscribe(newBaseAmount)
+        );
     }
 
     public BehaviorSubject<String> getTime() { return time; }
-    public BehaviorSubject<List<Currency>> getCurrencies() { return currencyList; }
+
+    public BehaviorSubject<List<Currency>> getCurrencies() { return currencyList;}
+    public static BehaviorSubject<Object> getNewBaseAmount() { return newBaseAmount;}
+
+    private static String purge(String xxx){
+        xxx = xxx.replace(" ", "");
+        xxx = xxx.replace(",", "");
+        xxx = xxx.replace(".", "");
+        String ret = "";
+        for(int i = 0; i < xxx.length(); i++){
+            if(Character.isDigit(xxx.charAt(i))) ret += xxx.charAt(i);
+        }
+        return ret;
+    }
+    public static boolean validate(String xxx) {
+        //Todo implement 'nicer' validation
+        if (xxx.equals("")) return true;
+        for (int i = 0; i < xxx.length(); i++) {
+            if (!Character.isDigit(xxx.charAt(i))) return false;
+        }
+        return true;
+    }
+
+    public static boolean broadcastNewValue(String newValue) {
+        String purged = purge(newValue);
+        if (validate(purged)) {
+            foreignCurrencyBus.send(purged.equals("") ? "0" : purged);
+            return true;
+        }
+        return false;
+    }
 }
--- app/src/main/java/is/robertreynisson/iskcurrency/presenter_layer/Currency/CurrencyRecyclerAdapter.java
+++ app/src/main/java/is/robertreynisson/iskcurrency/presenter_layer/Currency/CurrencyRecyclerAdapter.java
@@ -2,20 +2,16 @@ package is.robertreynisson.iskcurrency.presenter_layer.Currency;
 
 import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
-import is.robertreynisson.iskcurrency.ISKCurrency;
 import is.robertreynisson.iskcurrency.R;
 import is.robertreynisson.iskcurrency.databinding.CurrencyItemBinding;
+import is.robertreynisson.iskcurrency.domain_layer.CurrencyViewModel;
 import is.robertreynisson.iskcurrency.presenter_layer.models.Currency;
-import rx.android.schedulers.AndroidSchedulers;
 
 /**
  * Created by robert on 10.2.2016.
@@ -37,11 +33,6 @@ public class CurrencyRecyclerAdapter extends RecyclerView.Adapter<CurrencyRecycl
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        CurrencyView.foreignCurrencyBus.toObserverable().throttleLast(2, TimeUnit.SECONDS, AndroidSchedulers.mainThread()).subscribe(o -> currencies.get(position).baseCurrencyAmount = Double.parseDouble(o.toString()));
-
-        EditText e = (EditText) holder.getBinding().getRoot().findViewById(R.id.currency_item_edit_text);
-        e.addTextChangedListener(holder.getCurrencyChanged(currencies.get(position)));
-
         holder.bindConnection(currencies.get(position));
     }
 
@@ -55,45 +46,31 @@ public class CurrencyRecyclerAdapter extends RecyclerView.Adapter<CurrencyRecycl
     // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private final EditText currencyEditText;
-        private boolean currecyHasFocus = false;
+        private Currency currency;
         // each data item is just a string in this case
-        public CurrencyItemBinding currency;
+        public CurrencyItemBinding currencyItemBinding;
 
-        public ViewHolder(CurrencyItemBinding currency) {
-            super(currency.getRoot());
-            this.currencyEditText = (EditText) currency.getRoot().findViewById(R.id.currency_item_edit_text);
-            this.currencyEditText.setOnFocusChangeListener((v, hasFocus) -> currecyHasFocus = hasFocus);
-            this.currency = currency;
+        public ViewHolder(CurrencyItemBinding currencyItemBinding) {
+            super(currencyItemBinding.getRoot());
+            currencyEditText = (EditText) currencyItemBinding.getRoot().findViewById(R.id.currency_item_edit_text);
+            this.currencyItemBinding = currencyItemBinding;
         }
 
         public void bindConnection(Currency currency) {
-            this.currency.setCurrency(currency);
+            CurrencyViewModel.getNewBaseAmount().subscribe(o -> updateItem(o.toString()));
+            this.currency = currency;
+            this.currencyItemBinding.setCurrency(currency);
+        }
+
+        private void updateItem(String newAmount) {
+            this.currency.baseCurrencyAmount = Double.parseDouble(newAmount);
+            this.currencyItemBinding.setCurrency(currency);
         }
 
         public CurrencyItemBinding getBinding() {
-            return this.currency;
+            return this.currencyItemBinding;
         }
 
-        public TextWatcher getCurrencyChanged(Currency currency) {
-            return new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                }
-
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    if(!currecyHasFocus) return;
-                    currencyEditText.setError(null);
-                    if(!CurrencyView.foreignCurrencyChanged(currency, currencyEditText.getText().toString())) currencyEditText.setError(ISKCurrency.getInstance().getResources().getString(R.string.input_validation_error_digits));
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {
-
-                }
-            };
-        }
     }
 
 }
